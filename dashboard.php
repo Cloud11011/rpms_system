3912bb49e87ece75f83045bff0ac900aa62401c9
Cloud11011 <127287034+Cloud11011@users.noreply.github.com>
2026-09-24 19:15:59 +0800

    Wire staff profile and activity menu links

--- dashboard.php
+++ dashboard.php
@@ -65,9 +65,9 @@ try {
 <i class="fa-solid fa-chevron-down dropdown-arrow"></i>
 </div>
 <div class="profile-menu" id="profileMenu">
-<a href="#"><i class="fa-solid fa-user-gear"></i> Profile</a>
-<a href="#"><i class="fa-solid fa-gear"></i> Settings</a>
-<a href="#"><i class="fa-solid fa-sliders"></i> Activity Logs</a>
+<a href="account.php#profile"><i class="fa-solid fa-user-gear"></i> Profile</a>
+<a href="account.php#security"><i class="fa-solid fa-gear"></i> Settings</a>
+<a href="account.php#activity"><i class="fa-solid fa-sliders"></i> Activity Logs</a>
 <hr>
 <a href="logout.php" class="logout-btn"><i class="fa-solid fa-right-from-bracket"></i> Log Out</a>
 </div>
